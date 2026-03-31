6b0aa6ad5aa103ddef1beb597cf9002b0ccd4dce
Sridaran Thiagarajan <sridarant@gmail.com>
2026-03-31 21:40:10 +0530

    v20

--- src/pages/Insights.jsx
+++ src/pages/Insights.jsx
@@ -215,6 +215,7 @@ export default function Insights() {
           </div>
         ) : (
           <>
+            <>
             {/* OVERVIEW */}
             {activeTab === 'overview' && (
               <>
